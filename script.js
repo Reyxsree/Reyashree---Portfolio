8ac39f7e438e552a954aee90cbd7551b641011a5
Reyxsree <reyashreesathyanarayanan04@gmail.com>
2024-09-07 14:23:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: ["Frontend Developer", "Computer Science Student", "Graphic Designer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
